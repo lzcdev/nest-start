b5bbe6c492a274c9d3e8ebabf56c1b69c44b3c29
lzcdev <lzc2134@gmail.com>
2020-08-31 21:49:49 +0800

    feat: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
-import { HelloModule } from './modules/hello/hello.module'
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { HelloModule } from './modules/hello/hello.module';
 
 @Module({
-  imports: [HelloModule]
+  imports: [HelloModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('hello');
+  }
+}
