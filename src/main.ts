3f2b5a234f02adec2809525ddfc1751c2658c48f
lzcdev <lzc2134@gmail.com>
2020-09-01 14:13:20 +0800

    feat: add exception filters, module or global mode

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局过滤器
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
   const options = new DocumentBuilder()
     .setTitle('nest-start api document')
     .setDescription('nest-start project api document')
